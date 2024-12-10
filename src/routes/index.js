d144adf35593b0220eb5df6edb065d3836069046
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-12-10 16:00:04 +0700

    event root

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 // import authRouter from './auth.js';import 
 import authRouter from './auth.routes.js';
+import eventRouter from "./event.routes.js";
 
 
 const rootRouter = Router();
 
 rootRouter.use("/auth", authRouter);
+rootRouter.use("/events", eventRouter);
 
 export default rootRouter;
