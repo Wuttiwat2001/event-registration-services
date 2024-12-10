72072338ed6e1d0a8f432a6a94773c95aafc2b14
KhunCaptain <captenlnw_za@hotmail.com>
2024-12-10 19:15:06 +0700

    findOne event

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -61,7 +61,23 @@ const findAll = async (req, res) => {
   }
 };
 
-const findOne = async (req, res) => {};
+const findOne = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const event = await Event.findById(id).populate(
+      "createdBy",
+      "firstName lastName"
+    );
+    if (!event) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Event not found" });
+    }
+    res.status(200).json({ success: true, data: event });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 const update = async (req, res) => {
   try {
