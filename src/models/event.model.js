84ba5b3c267cc977f7b35b5e1f5d140623ed3da0
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-12-10 15:04:25 +0700

    Create Event model schema with Mongoose

---
+++ src/models/event.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const eventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      require: true,
+    },
+    description: {
+      type: String,
+      require: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return value > this.startDate;
+        },
+        message: "End date must be after start date",
+      },
+    },
+    location: {
+      type: String,
+      require: true,
+    },
+    totalSeats: {
+      type: Number,
+      required: true,
+    },
+    remainingSeats: {
+      type: Number,
+      required: true,
+    },
+    registeredUsers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const eventModel = mongoose.models.event || mongoose.model("event", eventSchema);
+export default eventModel;
+
