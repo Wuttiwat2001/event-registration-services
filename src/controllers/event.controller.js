52b1fec2232946d25f3414a631bd7a010af1dd54
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-12-10 16:43:59 +0700

    findAll event and remove event

--- src/controllers/event.controller.js
+++ src/controllers/event.controller.js
@@ -39,24 +39,46 @@ const create = async (req, res) => {
 
     const savedEvent = await event.save();
 
-    res
-      .status(201)
-      .json({
-        success: true,
-        data: savedEvent,
-        message: "Event created successfully.",
-      });
+    res.status(201).json({
+      success: true,
+      data: savedEvent,
+      message: "Event created successfully.",
+    });
   } catch (error) {
     res.status(500).json({ success: false, message: error.message });
   }
 };
 
-const findAll = async (req, res) => {};
+const findAll = async (req, res) => {
+  try {
+    const events = await Event.find({ deletedAt: null }).populate("createdBy", "firstName lastName");
+    res.status(200).json({ success: true, data: events });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 const findOne = async (req, res) => {};
 
 const update = async (req, res) => {};
 
-const remove = async (req, res) => {};
+const remove = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const event = await Event.findById(id);
+
+    if (!event) {
+      return res.status(404).json({ success: false, message: "Event not found" });
+    }
+
+    event.deletedAt = new Date();
+    await event.save();
+
+    res.status(200).json({ success: true, message: "Event marked as deleted" });
+
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
 
 export { create, findAll, findOne, update, remove };
