c5ba0d73abdc2829012ad7d9d698aca3617d8ceb
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-12-10 13:19:00 +0700

    Create root router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import { PORT } from "./secrets.js";
 import connectDB from "./config/mongodb.js";
+import rootRouter from "./routes/index.js";
 
 const app = express();
 
@@ -12,6 +13,9 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.use("/api", rootRouter);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
