806b3bba26544f7b353cccfcc6db3add9e6c5704
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-12-10 12:58:55 +0700

    Update model User

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,19 +1,33 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    require: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    firstName: {
+      type: String,
+      require: true,
+    },
+    lastName: {
+      type: String,
+      require: true,
+    },
+    phone: {
+      type: String,
+      require: true,
+      unique: true,
+    },
   },
-  lastName: {
-    type: String,
-    require: true,
-  },
-  phone: {
-    type: Number,
-    require: true,
-  },
-  createdAt: { type: Date, default: Date.now },
-});
+  { timestamps: true }
+);
 
-export default mongoose.model("User", userSchema);
+const userModel = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default userModel;
