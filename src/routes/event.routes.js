cc4d0bf7386ab5bb6a690e679cae9cd27967e177
KhunCaptain <captenlnw_za@hotmail.com>
2024-12-16 22:17:31 +0700

    validation create event

--- src/routes/event.routes.js
+++ src/routes/event.routes.js
@@ -6,10 +6,12 @@ import {
   update,
   remove,
 } from "../controllers/event.controller.js";
+import validateResult from "../middlewares/validation.middleware.js";
+import { validateCreate } from "../validators/event.validator.js";
 
 const eventRouter = Router();
 
-eventRouter.post("/", create);
+eventRouter.post("/", validateResult, validateCreate, create);
 eventRouter.post("/findAll", findAll);
 eventRouter.get("/:id", findOne);
 eventRouter.put("/update/:id", update);
---
+++ src/validators/event.validator.js
@@ -0,0 +1,13 @@
+import { body } from "express-validator";
+
+const validateCreate = [
+  body("title").notEmpty().withMessage("Title is required"),
+  body("description").notEmpty().withMessage("Description is required"),
+  body("location").notEmpty().withMessage("Location is required"),
+  body("totalSeats").notEmpty().withMessage("Total seats is required"),
+  body("remainingSeats").notEmpty().withMessage("Remaining seats is required"),
+  body("createdBy").notEmpty().withMessage("Created by is required"),
+];
+
+
+export { validateCreate };
