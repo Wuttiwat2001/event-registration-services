f96d18a998d338bef287e2487b39dd20d3fdbee2
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-12-17 16:12:38 +0700

    update validate event

--- src/validators/event.validator.js
+++ src/validators/event.validator.js
@@ -1,21 +1,56 @@
 import { body } from "express-validator";
 
 const validateCreate = [
-  body("title").notEmpty().withMessage("Title is required"),
-  body("description").notEmpty().withMessage("Description is required"),
-  body("location").notEmpty().withMessage("Location is required"),
+  body("title")
+    .notEmpty()
+    .withMessage("Title is required")
+    .matches(/^[A-Za-z0-9\s]+$/)
+    .withMessage(
+      "Title should contain only alphanumeric characters and spaces"
+    ),
+  body("description")
+    .notEmpty()
+    .withMessage("Description is required")
+    .matches(/^[A-Za-z0-9\s]+$/)
+    .withMessage(
+      "Description should contain only alphanumeric characters and spaces"
+    ),
+  body("location")
+    .notEmpty()
+    .withMessage("Location is required")
+    .matches(/^[A-Za-z0-9\s]+$/)
+    .withMessage(
+      "Location should contain only alphanumeric characters and spaces"
+    ),
   body("totalSeats").notEmpty().withMessage("Total seats is required"),
   body("remainingSeats").notEmpty().withMessage("Remaining seats is required"),
   body("createdBy").notEmpty().withMessage("Created by is required"),
 ];
 
 const validateUpdate = [
-  body("title").notEmpty().withMessage("Title is required"),
-  body("description").notEmpty().withMessage("Description is required"),
-  body("location").notEmpty().withMessage("Location is required"),
+  body("title")
+    .notEmpty()
+    .withMessage("Title is required")
+    .matches(/^[A-Za-z0-9\s]+$/)
+    .withMessage(
+      "Title should contain only alphanumeric characters and spaces"
+    ),
+  body("description")
+    .notEmpty()
+    .withMessage("Description is required")
+    .matches(/^[A-Za-z0-9\s]+$/)
+    .withMessage(
+      "Description should contain only alphanumeric characters and spaces"
+    ),
+  body("location")
+    .notEmpty()
+    .withMessage("Location is required")
+    .matches(/^[A-Za-z0-9\s]+$/)
+    .withMessage(
+      "Location should contain only alphanumeric characters and spaces"
+    ),
   body("totalSeats").notEmpty().withMessage("Total seats is required"),
   body("remainingSeats").notEmpty().withMessage("Remaining seats is required"),
-  body("createdBy").notEmpty().withMessage("Created by is required"),
 ];
 
 export { validateCreate, validateUpdate };
