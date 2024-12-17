35dc6320272d05497d7053172a0aacf59e5fff1c
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-12-17 16:01:46 +0700

    fix validate register

--- src/validators/auth.validator.js
+++ src/validators/auth.validator.js
@@ -8,12 +8,22 @@ const validateLogin = [
 const validateRegister = [
   body("username")
     .isLength({ min: 4 })
-    .withMessage("Username should be at least 4 characters"),
+    .withMessage("Username should be at least 4 characters")
+    .matches(/^[A-Za-z0-9]+$/)
+    .withMessage("Username should contain only alphanumeric characters"),
   body("password")
     .isLength({ min: 5 })
     .withMessage("Password should be at least 5 characters"),
-  body("firstName").notEmpty().withMessage("First name is required"),
-  body("lastName").notEmpty().withMessage("Last name is required"),
+  body("firstName")
+    .notEmpty()
+    .withMessage("First name is required")
+    .matches(/^[A-Za-z]+$/)
+    .withMessage("First name should contain only alphabetic characters"),
+  body("lastName")
+    .notEmpty()
+    .withMessage("Last name is required")
+    .matches(/^[A-Za-z]+$/)
+    .withMessage("Last name should contain only alphabetic characters"),
   body("phone")
     .isLength({ min: 10, max: 10 })
     .withMessage("Phone number should be exactly 10 digits"),
