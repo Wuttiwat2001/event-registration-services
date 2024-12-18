d4f7efdad0afdab5c06b1f6952f69bb029d56099
KhunCaptain <wuttiwat.p@poontana.co.th>
2024-12-18 09:13:27 +0700

    expires token

--- src/helpers/generateToken.js
+++ src/helpers/generateToken.js
@@ -2,8 +2,7 @@ import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '../secrets.js';
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, JWT_SECRET);
+  return jwt.sign({ id }, JWT_SECRET, { expiresIn: '1d' }); 
 }
-
 
 export default generateToken;
